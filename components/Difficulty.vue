bf516b88568723d13f0420d7c17acc1a96bed7c1
J-Allyson30 <jeanallyson.junsay@tup.edu.ph>
2023-04-10 14:38:51 +0800

    pause game

--- components/Difficulty.vue
+++ components/Difficulty.vue
@@ -10,14 +10,36 @@
                         <img class="btn" src="../assets/web/playisclicked/MEDIUM.png" @click="$emit('pageS',1)"><br>
                         <img class="btn" src="../assets/web/HOME/PLAY.png" @click="$emit('user-name','')">
                     </div>
-            <div v-else>
-                <Card :items="shuffleItems"/>
-                
-            </div>
-            
+                      <div v-else class="pausewindow">
+                            <Card :items="shuffleItems"/>
+                            
+                        <br><button class="pausebutton" @click="showModal = true">Pause Game</button>
+                    <div v-if="showModal" class="modal">
+                        <div class="modal-content">
+                                <!-- <h2>Game Paused</h2> -->
+                                <!-- <img class="pause" src="../assets/web/paused/BG.png" @click="showModal = true"><br> -->
+                                <!-- <img src="../assets/web/paused/BG.png" alt=""> -->
+                                <div class="modal-image"> 
+                                    <div class="resume">
+                                         <img src="../assets/web/paused/PAUSE.png">
+                                    </div>
+                                    <p><i>The game is paused. Click back button to resume. Click close button to exit</i></p>
+                            <div class="modal-buttons">
+                                <img class="back" src="../assets/web/paused/BACK.png" @click="showModal = false"><br>
+                                <img class="back" @click="$emit('pageS',0)" src="../assets/web/playisclicked/close.png">
+                                <!-- <button class="no" @click="showModal = false">No</button>
+                                <button class="yes" @click="resumeGame()">Yes</button> -->
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                      </div>
+                      
                 </div>
             </div>
+            
         </div>
+        
     </div>
 </template>
 
@@ -25,6 +47,7 @@
     export default {
         data(){
             return{
+                
                 items: [
                     {name: 'APPLE', image: 'APPLE.png'},
                     {name: 'BANANA', image: 'BANANA.png'},
@@ -38,7 +61,8 @@
                 gameStarted: false,
                 randomItems: [],
                 shuffleItems: [],
-                size: null
+                size: null,
+                showModal: false,
             }
         },
         methods: {
@@ -58,6 +82,37 @@
                 this.shuffleItems.sort(() => Math.random() -0.5)
                 return this.shuffleItems
             },
+            resumeGame() {
+            // Code to resume the game
+            this.showModal = false;
+    },
         }
     }
 </script>
+
+<!-- <style>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 9999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+.modal-buttons {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
+}
+</style> -->
